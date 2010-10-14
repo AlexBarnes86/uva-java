048a4ee34455611607db2aa2a9799b7c29897d19
Alex Barnes <AlexBarnes86@gmail.com>
2010-10-13 23:00:17 -0500

    Solved 755

--- Unsolved/Problem755.java
+++ Unsolved/Problem755.java
@@ -1,85 +1,80 @@
-//too slow, needs c++ on UVa - bastards
 package AOAPC.vol1.SortingSearching;
 
-import java.io.File;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.TreeMap;
 
 public class Problem755 {
-	public static String getNumber(String line) {
-		StringBuilder sb = new StringBuilder();
+	public static String standardize(String line) {
+		StringBuffer sb = new StringBuffer();
 		for(int i = 0; i < line.length(); ++i) {
-			switch(line.charAt(i)) {
-			case 'A': case 'B': case 'C': case 'a': case 'b': case 'c':
+			char ch = line.charAt(i);
+			switch(ch) {
+			case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '0':
+				sb.append(ch);
+				break;
+			case 'A': case 'B': case 'C':
 				sb.append('2');
 				break;
-			case 'D': case 'E': case 'F': case 'd': case 'e': case 'f':
+			case 'D': case 'E': case 'F':
 				sb.append('3');
 				break;
-			case 'G': case 'H': case 'I': case 'g': case 'h': case 'i':
+			case 'G': case 'H': case 'I':
 				sb.append('4');
 				break;
-			case 'J': case 'K': case 'L': case 'j': case 'k': case 'l':
+			case 'J': case 'K': case 'L':
 				sb.append('5');
 				break;
-			case 'M': case 'N': case 'O': case 'm': case 'n': case 'o':
+			case 'M': case 'N': case 'O':
 				sb.append('6');
 				break;
-			case 'P': case 'R': case 'S': case 'p': case 'r': case 's':
+			case 'P': case 'R': case 'S':
 				sb.append('7');
 				break;
-			case 'T': case 'U': case 'V': case 't': case 'u': case 'v':
+			case 'T': case 'U': case 'V':
 				sb.append('8');
 				break;
-			case 'W': case 'X': case 'Y': case 'w': case 'x': case 'y':
+			case 'W': case 'X': case 'Y':
 				sb.append('9');
-				break;
-			case '1': case '2': case '3': case '4': case '5':
-			case '6': case '7': case '8': case '9': case '0':
-				sb.append(line.charAt(i));
-				break;
-			default:
 				break;
 			}
 		}
 		return sb.toString();
 	}
 	
-	public static void main(String[] args) throws Exception {
-		//long start = getSystemTime();
-		long start = System.currentTimeMillis();
-		Scanner sc = new Scanner(new File("Problem755.input"));
-		int N = Integer.parseInt(sc.nextLine());
-		sc.nextLine(); //eat blank line
+	public static void solve(BufferedReader br) throws Exception {
+		int N = Integer.parseInt(br.readLine());
+		TreeMap<String, Integer> hm = new TreeMap<String, Integer>();
 		for(int i = 0; i < N; ++i) {
-			HashMap<String, Integer> numbers = new HashMap<String, Integer>();
-			int M = Integer.parseInt(sc.nextLine());
-			HashSet<String> collisions = new HashSet<String>();
-			for(int j = 0; j < M; ++j) {
-				String number = getNumber(sc.nextLine());
-				if(numbers.containsKey(number)) {
-					numbers.put(number, numbers.get(number)+1);
-					collisions.add(number);
-				}
-				else
-					numbers.put(number, 1);
+			String line = br.readLine();
+			String number = standardize(line);
+			if(hm.containsKey(number)) {
+				hm.put(number, hm.get(number)+1);
+			} else {
+				hm.put(number, new Integer(1));
 			}
-			ArrayList<String> keys = new ArrayList<String>();
-			for(Iterator<String> itr = collisions.iterator(); itr.hasNext(); ) {
-				keys.add(itr.next());
-			}
-			Collections.sort(keys);
-			for(int j = 0; j < keys.size(); ++j) {
-				String num = keys.get(j);
-				int collide = numbers.get(num);
-				System.out.println(num.substring(0, 3)+"-"+num.substring(3)+" "+collide);
-			}
-			if(keys.size() == 0)
-				System.out.println("No duplicates.");
-			System.out.println();
-			if(sc.hasNext())
-				sc.nextLine(); //eat blank line
 		}
-		//System.out.println("System Time: " + (getSystemTime()-start));
-		//System.out.println("Running Time: " + (System.currentTimeMillis()-start));
+		boolean output = false;
+		for(String number : hm.keySet()) {
+			int repeats = hm.get(number).intValue();
+			if(repeats > 1) {
+				System.out.println(number.substring(0, 3) + '-' + number.substring(3) + " " + repeats);
+				output = true;
+			}
+		}
+		if(!output) {
+			System.out.println("No duplicates.");
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for(int i = 0; i < N; ++i) {
+			br.readLine(); //read blank line before each test case
+			solve(br);
+			if(i != N-1)
+				System.out.println(); //print blank line between datasets
+		}
 	}
 }
