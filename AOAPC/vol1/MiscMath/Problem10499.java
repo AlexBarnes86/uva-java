8cb2813ab1460fa9d994a682e14735bf0151321f
Alex Barnes <AlexBarnes86@gmail.com>
2010-10-19 23:57:45 -0500

    Solved Problem10499 The Land of Justice

---
+++ AOAPC/vol1/MiscMath/Problem10499.java
@@ -0,0 +1,28 @@
+package AOAPC.vol1.MiscMath;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Problem10499 {
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = "";
+
+		long n;
+		
+		while((line = br.readLine()) != null) {
+			String [] tokens = line.trim().split("\\s+");
+			for(String str : tokens) {
+				n = Long.parseLong(str);
+				if(n < 0)
+					return;
+				if(n == 1)
+					System.out.println("0%");
+				else {
+					n = n * 25;
+					System.out.println(n + "%");
+				}
+			}
+		}
+	}
+}
