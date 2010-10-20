49e2ae83f3ec2967232f4f15cfd138d55ef649d3
Alex Barnes <AlexBarnes86@gmail.com>
2010-10-19 22:42:16 -0500

    Solved Problem253 Cube Painting

--- AOAPC/vol1/MiscMath/Problem253.java
+++ AOAPC/vol1/MiscMath/Problem253.java
@@ -4,16 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Problem253 {
-	public static int findAxis(String c1, String c2) {
-		if(c1.charAt(0) == c2.charAt(0) && c1.charAt(5) == c2.charAt(5))
-			return 1;
-		if(c1.charAt(1) == c2.charAt(1) && c1.charAt(4) == c2.charAt(4))
-			return 2;
-		if(c1.charAt(2) == c2.charAt(2) && c1.charAt(3) == c2.charAt(3))
-			return 3;
-		return -1;
-	}
-	
 	public static String rotateRight(String c) {
 		return "" + c.charAt(0) + c.charAt(2) + c.charAt(4) + c.charAt(1) + c.charAt(3) + c.charAt(5);
 	}
@@ -27,35 +17,41 @@ public class Problem253 {
 	}
 	
 	public static boolean solve(String c1, String c2) {
-		String cube = c1;
-		for(int i = 0; i < 4; ++i) {
-			int axis = findAxis(cube, c2);
-			if(axis > 0) {
-				for(int j = 0; j < 4; ++j) {
-					if(cube.equals(c2))
-						return true;
-					switch(axis) {
-					//vertical axis
-					case 1: cube = rotateRight(cube);
-						break;
-					//into page axis
-					case 2: cube = rotateClockwise(cube);
-						break;
-					//horizontal axis
-					case 3: cube = rotateDown(cube);
-						break;
-					}
-				}
+		for(int i = 0; i < 4; ++i) {//rotate right
+			for(int j = 0; j < 4; ++j) {//rotate down
+				if(c1.equals(c2))
+					return true;
+				c1 = rotateDown(c1);
+			}
+			for(int j = 0; j < 4; ++j) {//rotate clockwise
+				if(c1.equals(c2))
+					return true;
+				c1 = rotateClockwise(c1);
 			}
 			c1 = rotateRight(c1);
-			cube = c1;
 		}
+
 		return false;
 	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line = "";
+		
+//		String abc = "abcdef";
+//		for(int i = 0; i < 4; ++i) {
+//			abc = rotateRight(abc);
+//		}
+//		System.out.println(abc);
+//		for(int i = 0; i < 4; ++i) {
+//			abc = rotateDown(abc);
+//		}
+//		System.out.println(abc);
+//		for(int i = 0; i < 4; ++i) {
+//			abc = rotateClockwise(abc);
+//		}
+//		System.out.println(abc);
+		
 		while((line = br.readLine()) != null) {
 			String cube1 = line.substring(0, 6);
 			String cube2 = line.substring(6);
