2c1c801ba1b0e4ecc9955b077c72498252bfd85e
Alex Barnes <AlexBarnes86@gmail.com>
2010-11-04 20:29:14 -0500

    Solved Problem442 Matrix Chain Multiplication Working on Problem10050 Hartalls

---
+++ AOAPC/src/vol2/Lists/Problem10050.java
@@ -0,0 +1,5 @@
+package vol2.Lists;
+
+public class Problem10050 {
+
+}
--- AOAPC/src/vol2/Lists/Problem442.java
+++ AOAPC/src/vol2/Lists/Problem442.java
@@ -1,4 +1,5 @@
 package vol2.Lists;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.InputStreamReader;
@@ -29,54 +30,63 @@ public class Problem442 {
 	}
 	
 	public static void solve(HashMap<Character, Matrix> matrices, String line) {
-		LinkedList<Character> stack = new LinkedList<Character>();
-		Matrix temp = null;
+		LinkedList<String> stack = new LinkedList<String>();
+		HashMap<Integer, Matrix> lookup = new HashMap<Integer, Matrix>();
+		int lookupct = 0;
+		String token = "";
+		char ch;
+
 		int total = 0;
 		
 		for(int i = 0; i < line.length(); ++i) {
-			char ch = line.charAt(i);
+			ch = line.charAt(i);
 			if(ch != ')') {
-				stack.push(ch);
-				System.out.println("DEBUG: adding " + ch);
+				stack.push(ch + "");
+				//System.out.println("DEBUG: adding " + ch);
 			}
 			else {
 				LinkedList<Matrix> queue = new LinkedList<Matrix>();
-				System.out.println("DEBUG: stack " + stack);
-				while((ch = stack.pop()) != '(') {
+				//System.out.println("DEBUG: stack " + stack);
+
+				while(!(token = stack.pop()).equals("(")) {
+					ch = token.charAt(0);
 					//System.out.println("DEBUG: popping " + ch);
 					if(ch == '*') {
-						queue.addFirst(temp);
+						queue.addFirst(lookup.get(Integer.parseInt(token.replace('*', ' ').trim())));
+						lookupct--;
 					}
 					else {
 						queue.addFirst(matrices.get(ch));
 					}
 				}
 				
-				System.out.println("DEBUG: queue " + queue);
+				//System.out.println("DEBUG: queue " + queue);
 				
 				Matrix lhs = queue.removeFirst();
 				while(!queue.isEmpty()) {
 					Matrix rhs = queue.removeFirst();
 					if(lhs.N != rhs.M) {
-						System.out.print("DEBUG: 1st ");
+						//System.out.print("DEBUG: 1st ");
 						System.out.println("error");
 						return;
 					}
 					total += lhs.countMults(rhs);
 					lhs = new Matrix(lhs.M, rhs.N);
 				}
-				stack.push('*');
-				temp = lhs;
+				stack.push("*" + lookupct + "*");
+				lookup.put(lookupct, lhs);
+				lookupct++;
 			}
 		}
 		
 		//after all parens dealt with
 		LinkedList<Matrix> queue = new LinkedList<Matrix>();
-		char ch;
+
 		while(!stack.isEmpty()) {
-			ch = stack.pop();
+			token = stack.pop();
+			ch = token.charAt(0);
 			if(ch == '*') {
-				queue.addFirst(temp);
+				queue.addFirst(lookup.get(Integer.parseInt(token.replace('*', ' ').trim())));
 			}
 			else {
 				queue.addFirst(matrices.get(ch));
@@ -86,7 +96,7 @@ public class Problem442 {
 		while(!queue.isEmpty()) {
 			Matrix rhs = queue.removeFirst();
 			if(lhs.N != rhs.M) {
-				System.out.print("DEBUG: 2nd ");
+				//System.out.print("DEBUG: 2nd ");
 				System.out.println("error");
 				return;
 			}
@@ -97,8 +107,8 @@ public class Problem442 {
 	}
 	
 	public static void main(String[] args) throws Exception {
-		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedReader br = new BufferedReader(new FileReader("input/vol2/Problem442.in"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedReader br = new BufferedReader(new FileReader("input/vol2/Problem442.in"));
 		String line = "";
 		
 		int N = Integer.parseInt(br.readLine());
