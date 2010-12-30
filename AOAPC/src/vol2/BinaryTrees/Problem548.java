c47b3ac6909f97573c8da937b92fd34f5af49691
Alex Barnes <AlexBarnes86@gmail.com>
2010-12-29 18:38:12 -0600

    Works but TLE, input probably contains a linked list of 10k elements. subList is very slow due to returning generic List type.

--- AOAPC/src/vol2/BinaryTrees/Problem548.java
+++ AOAPC/src/vol2/BinaryTrees/Problem548.java
@@ -3,13 +3,14 @@ package vol2.BinaryTrees;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Problem548 {
 	static class Node {
 		public long val;
 		public Node left, right;
+		public long psum;
 		
 		public Node(long val) {
 			this.val = val;
@@ -28,52 +29,71 @@ public class Problem548 {
 			inorder(this, sb);
 			return sb.toString();
 		}
+		
+		private static void getLeaves(Node n, LinkedList<Node> leaves, long sum) {
+			if(n == null)
+				return;
+			
+			n.psum = sum+n.val;
+			
+			getLeaves(n.left, leaves, n.psum);
+			if(n.left == null && n.right == null)
+				leaves.add(n);
+			getLeaves(n.right, leaves, n.psum);
+		}
+		
+		public LinkedList<Node> getLeaves() {
+			LinkedList<Node> ll = new LinkedList<Node>();
+			getLeaves(this, ll, 0);
+			return ll;
+		}
+		
+		public Node largestPathLeaf() {
+			LinkedList<Node> leaves = getLeaves();
+			Node min = leaves.get(0);
+			//System.out.print("leaves: ");
+			for(Node n : leaves) {
+				//System.out.print(n.val + ":" + n.psum + " ");
+				if(n.psum < min.psum)
+					min = n;
+			}
+			//System.out.println();
+			return min;
+		}
 	}
 	
 	static class Tree {
 		Node root;
 		
-		private ArrayList<Long> inorder, postorder;
-		
-		private Node build(int inLeft, int inRight, int postLeft, int postRight) {
-			if(inLeft > inRight) {
-				if(postLeft == postRight)
-					return new Node(inorder.get(postLeft));
-				else
-					return null;
-			}
-			if(postLeft > postRight) {
-				if(inLeft == inRight)
-					return new Node(inorder.get(inLeft));
-				else
-					return null;
-			}
-			if(inLeft == inRight)
-				return new Node(inorder.get(inLeft));
-			if(postLeft == postRight)
-				return new Node(inorder.get(postLeft));
+		private Node build(List<Long> inorder, List<Long> postorder) {
+			if(postorder.isEmpty())
+				return null;
 			
-			long val = postorder.get(postRight);
-			Node node = new Node(val);
+			Node node = new Node(postorder.get(postorder.size()-1));
+			if(inorder.size() == 1)
+				return node;
 			
-			int idx = inorder.indexOf(val);
+			int idx = inorder.indexOf(node.val);
 			
-			node.left = build(inLeft, idx-1, postLeft, idx-1);
-			node.right = build(idx+1, inRight, idx, postRight-1);
+			if(idx == -1 || idx+1 > inorder.size())
+				return node;
+			
+			List<Long> left = inorder.subList(0, idx);
+			List<Long> right = inorder.subList(idx+1, inorder.size());
+			
+			node.left = build(left, postorder.subList(0, idx));
+			node.right = build(right, postorder.subList(idx, postorder.size()-1));
 			
 			return node;
 		}
 		
-		public Tree(ArrayList<Long> inorder, ArrayList<Long> postorder) {
-			this.inorder = inorder;
-			this.postorder = postorder;
-			root = build(0, inorder.size()-1, 0, postorder.size()-1);
+		public Tree(LinkedList<Long> inorder, LinkedList<Long> postorder) {
+			root = build(inorder, postorder);
 		}
 	}
 	
-	private static ArrayList<Long> getList(String str) {
-		ArrayList<Long> list = new ArrayList<Long>();
-		list.ensureCapacity(10000);
+	private static LinkedList<Long> getList(String str) {
+		LinkedList<Long> list = new LinkedList<Long>();
 		String [] tokens = str.split("\\s+");
 		for(String token : tokens) {
 			list.add(Long.parseLong(token));
@@ -81,17 +101,17 @@ public class Problem548 {
 		return list;
 	}
 	
-	public static int solve(String inorder, String postorder) {
-		ArrayList<Long> inList = getList(inorder), postList = getList(postorder);
+	public static long solve(String inorder, String postorder) {
+		LinkedList<Long> inList = getList(inorder), postList = getList(postorder);
 
 		Tree t = new Tree(inList, postList);
-		System.out.println(t.root);
-		return -1;
+		//System.out.println(t.root);
+		return t.root.largestPathLeaf().val;
 	}
 	
 	public static void main(String[] args) throws Exception {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedReader br = new BufferedReader(new FileReader("input/vol2/BinaryTrees/Problem548.in"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedReader br = new BufferedReader(new FileReader("input/vol2/BinaryTrees/Problem548.in"));
 		String line1 = "";
 		String line2 = "";
 		while((line1 = br.readLine()) != null) {
