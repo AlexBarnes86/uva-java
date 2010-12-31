54fd9d111ac8bc1ccdd13b380409518277e0f802
Alex Barnes <AlexBarnes86@gmail.com>
2010-12-31 16:07:53 -0600

    Working on Problem372, WA instead of TLE now

--- AOAPC/src/vol2/BinaryTrees/Problem327.java
+++ AOAPC/src/vol2/BinaryTrees/Problem327.java
@@ -3,120 +3,96 @@ package vol2.BinaryTrees;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedList;
 
-public class Problem327 {
-	public static LinkedList<String> tokenize(String line) {
-		line = line.replaceAll("(\\+\\+|--|[a-z])", " $1 ").trim();
-		String [] tokens = line.split("\\s+");
-		LinkedList<String> res = new LinkedList<String>();
-		Collections.addAll(res, tokens);
-		return res;
-	}
-	
-	public static int getVal(char ch) {
-		return ch - 'a' + 1;
-	}
-	
+public class Problem327 {		
 	public static void solve(String line) {
-		LinkedList<String> tokens = tokenize(line);
-		HashMap<Character, Integer> vars = new HashMap<Character, Integer>();
-		LinkedList<Character> postAdd = new LinkedList<Character>();
-		LinkedList<Character> postSub = new LinkedList<Character>();
+		boolean [] postAdd = new boolean[26];
+		boolean [] postSub = new boolean[26];
+		
+		int [] vars = new int [26];
+		for(int i = 0; i < vars.length; ++i)
+			vars[i] = -999;
 		
 		int total = 0;
-		char prev = ' ';
-		boolean sub = false; //used when we have a-++c
-		
-		while(!tokens.isEmpty()) {
-			String token = tokens.removeFirst();
-			if(token.equals("--")) {
-				if(prev >= 'a' && prev <= 'z') {
-					postSub.add(prev);
+		LinkedList<Character> stack = new LinkedList<Character>();
+
+		for(int i = 0; i < line.length(); ++i) {
+			char ch = line.charAt(i);
+			if(Character.isWhitespace(ch)) {
+				if(stack.peek() != null && stack.peek() != ' ')
+					stack.push(' ');
+				continue;
+			}
+			
+			if(ch == '-') {
+				if(stack.peek() != null && stack.peek() == '-') {
+					stack.pop();
+					if(stack.peek() != null && stack.peek() >= 'a' && stack.peek() <= 'z')
+						postSub[stack.pop()-'a'] = true;
+					else
+						stack.push('D');
+				} //not ++
+				else
+					stack.push('-');
+			}
+			else if(ch == '+') {
+				if(stack.peek() != null && stack.peek() == '+') {
+					stack.pop();
+					if(stack.peek() != null && stack.peek() >= 'a' && stack.peek() <= 'z')
+						postAdd[stack.pop()-'a'] = true;
+					else
+						stack.push('U');
+				} //not ++
+				else
+					stack.push('+');
+			}
+			else {
+				int pos = ch - 'a';
+				vars[pos] = pos+1;
+				
+				if(stack.peek() != null && stack.peek() == 'U') {
+					vars[pos]++;
+					stack.pop();
 				}
-				else {
-					prev = 'D';
-				}
-			}
-			else if(token.equals("++")) {
-				if(prev >= 'a' && prev <= 'z') {
-					postAdd.add(prev);
-				}
-				prev = 'U';
-			}
-			else if(token.equals("+")) {
-				prev = '+';
-				sub = false;
-			}
-			else if(token.equals("-")) {
-				prev = '-';
-				sub = true;
-			}
-			else { //a-z, appears at most once in expr
-				char ch = token.charAt(0);
-				if(vars.get(token) == null) {
-					vars.put(ch, getVal(ch));
+				else if(stack.peek() != null && stack.peek() == 'D') {
+					vars[pos]--;
+					stack.pop();
 				}
 				
-				switch(prev) {
-				case 'U':
-					vars.put(ch, vars.get(ch) + 1);
-					if(sub) {
-						total-=vars.get(ch);
-					}
-					else
-						total+=vars.get(ch);
-					break;
-				case 'D':
-					vars.put(ch, vars.get(ch) - 1);
-					if(sub) {
-						total-=vars.get(ch);
-					}
-					else
-						total+=vars.get(ch);
-					break;
-				case '+':
-					total+=vars.get(ch);
-					break;
-				case '-':
-					total-=vars.get(ch);
-					break;
-				case ' ':
-					total += vars.get(ch);
-					break;
+				if(stack.peek() != null && stack.peek() == ' ')
+					stack.pop();
+				
+				if(stack.peek() != null && stack.peek() == '-') {
+					stack.pop();
+					total-=vars[pos];
 				}
-				prev = ch;
+				else {//if(stack.peek() == '+')
+					if(stack.peek() != null)
+						stack.pop();
+					total+=vars[pos];
+				}
+				stack.push(ch);
 			}
-		}
-		
-		while(!postAdd.isEmpty()) {
-			char ch = postAdd.pop();
-			vars.put(ch, vars.get(ch)+1);
-		}
-		
-		while(!postSub.isEmpty()) {
-			char ch = postSub.pop();
-			vars.put(ch, vars.get(ch)-1);
+			
 		}
 		
 		System.out.println("Expression: " + line);
 		System.out.println("    value = " + total);
-		ArrayList<Character> sortedKeys = new ArrayList<Character>();
-		for(Character ch : vars.keySet()) {
-			sortedKeys.add(ch);
-		}
-		Collections.sort(sortedKeys);
-		for(Character ch : sortedKeys) {
-			System.out.println("    " + ch + " = " + vars.get(ch));
+		for(int i = 0; i < vars.length; ++i) {
+			if(vars[i] == -999)
+				continue;
+			if(postSub[i])
+				vars[i]--;
+			if(postAdd[i])
+				vars[i]++;
+			System.out.println("    " + (char)('a'+i) + " = " + vars[i]);
 		}
 	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		BufferedReader br = new BufferedReader(new FileReader("input/vol2/BinaryTrees/Problem327.in"));
+//		BufferedReader br = new BufferedReader(new FileReader("input/vol2/BinaryTrees/Problem327-2.in"));
 		String line = "";
 		while((line = br.readLine()) != null) {
 			solve(line);
