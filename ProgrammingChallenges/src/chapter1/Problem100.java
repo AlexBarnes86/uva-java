dd71477b459ba498a6ea39249b161005239cae19
Alex Barnes <AlexBarnes86@gmail.com>
2013-10-05 16:09:01 -0500

    Move Problem 100 - 3n + 1 to ProgrammingChallenges

--- ProgrammingChallenges/src/chapter1/Problem100.java
+++ ProgrammingChallenges/src/chapter1/Problem100.java
@@ -1,7 +1,6 @@
-package volume1;
+package chapter1;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 
 public class Problem100 {
@@ -11,10 +10,8 @@ public class Problem100 {
 		long val = i;
 		int ct = 1;
 		while(val != 1) {
-//			System.out.print(val + ", ");
 			if(val < 1000000 && LUT[(int)val-1] != 0) {
 				LUT[i-1] = ct + LUT[(int)val-1] -1;
-//				System.out.println("LUT\n" + i + ": " + ct + "\n");
 				return LUT[i-1];
 			}
 			if(val % 2 == 0)
@@ -24,32 +21,28 @@ public class Problem100 {
 			ct++;
 		}
 		LUT[i-1] = ct;
-//		System.out.println("1\n" + i + ": " + ct + "\n");
 		return ct;
 	}
 	
 	public static void main(String [] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//BufferedReader br = new BufferedReader(new FileReader("input/volume1/Problem100.in"));
 		String line = "";
-		
-		//test
-//		for(int i = 1; i < 1000001; ++i) {
-//			System.out.println(i + ": " + solve(i) + " " + solve(i));
-//		}
 		
 		while((line = br.readLine()) != null) {
 			String tokens[] = line.trim().split("\\s+");
 			int i = Integer.parseInt(tokens[0]);
 			int j = Integer.parseInt(tokens[1]);
-			int low = (i<j)?i:j;
-			int high = (i<j)?j:i;
+			int low = (i < j) ? i : j;
+			int high = (i < j) ? j : i;
 			int max = 0;
+			
 			for(int k = low; k <= high; k++) {
 				int cycles = solve(k);
-				if(cycles > max)
+				if(cycles > max) {
 					max = cycles;
+				}
 			}
+			
 			System.out.println(i + " " + j + " " + max);
 		}
 	}
